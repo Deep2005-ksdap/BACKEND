e8d1c51dc8ed5c79c41ca50f26fe36c0701304da
Deepanshu <dk128131@gmail.com>
2025-07-23 16:37:13 +0530

    cors origin set

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin: "http://localhost:5173", // only my frontend part
+    origin: "*", 
     credentials: true, //allow cookies if needed
   }
 ));
